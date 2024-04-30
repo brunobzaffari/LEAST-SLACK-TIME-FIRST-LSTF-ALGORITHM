18d900687f9dab6b412316a0474ed746bfc90621
brunobzaffari <51928971+brunobzaffari@users.noreply.github.com>
2024-04-30 10:48:27 -0300

    Update LSF.cpp

--- LSF.cpp
+++ LSF.cpp
@@ -14,10 +14,12 @@ public:
     Job(int t, int d, int p, int count) : TC(t), D(d), P(p), Count(count) {
         //cout << "Job created with T=" << TC << ", D=" << D << ", P=" << P << ", Count=" << Count << endl;
     }
+
     // Destrutor
     ~Job() {
         //cout << "Job destroyed" << endl;
     }
+
     int getTC(){ return TC; }
     int getD(){ return D; }
     int getP(){ return P; }
@@ -35,7 +37,6 @@ void printjobs(vector<Job> jobs){
 	}
 	cout << "-----------------------------------------------"<< endl;
 }
-
 void printlsf(vector<int> LSF){
 	cout << "-----------------------------------------------"<< endl;
 	for(int i=0; i<LSF.size(); i++){//Debug
@@ -43,7 +44,6 @@ void printlsf(vector<int> LSF){
 	}
 	cout << "-----------------------------------------------"<< endl;
 }
-
 int main() { 
     vector<int> LSF;
     vector<Job> jobs;
@@ -60,35 +60,47 @@ int main() {
     while(true){
         while (true) {
             if(str != ""){
-            	cout << "" << endl;
                 cout << str << endl;
-                premp =-1;
                 cout << premp << " " << troca << endl;
+                cout << "" << endl;
                 str = "";
                 troca = 0;
                 premp = 0;
                 min_value = -1;
                 LSF.clear(); 
-        	    jobs.clear(); 
-            }
+        	jobs.clear(); 
+                }
+            
             getline(cin, line);  // Lê a linha inteira
             istringstream iss(line);
             //cout << line << endl;
             if (iss >> N >> T) {// Checa se só existem dois números na linha
                 if (!(iss >> extra) && N > 0 && T > 0) {
-                    LSF.resize(N, 0); 
-                    //jobs.reserve(N); 
-                    jobs.resize(N, Job(0,0,0,0)); 
-                    /*for(int i=0; i<N; i++){//Debug
-                        cout << i << jobs[i].getTC() << " " << jobs[i].getD() << " " << jobs[i].getP() << endl;
-                    }*/
-                        break;  // Sai do loop se exatamente dois números foram lidos e não há mais nada depois
+                LSF.resize(N, 0); 
+        	//jobs.reserve(N); 
+        	jobs.resize(N, Job(0,0,0,0)); 
+        	/*for(int i=0; i<N; i++){//Debug
+		    cout << i << jobs[i].getTC() << " " << jobs[i].getD() << " " << jobs[i].getP() << endl;
+		}*/
+                    break;  // Sai do loop se exatamente dois números foram lidos e não há mais nada depois
                 }
                 else if (N == 0 && T == 0) {
                     return 0;
                 }
-            }            
+            }
+            
         }
+        
+        /*for (int i = 0; i < N; i++) {// Insere jobs
+            int TC, D, P;
+            if (getline(cin, line)) {
+                istringstream jobStream(line);
+                if (jobStream >> TC >> D >> P) {
+                    jobs.emplace_back(Job(TC, D, P, TC));
+                }
+            }
+            cout << TC << " " << D << " " << P << endl;
+        }*/
         for (int i = 0; i < N; i++) {
             int TC, D, P;
             getline(cin, line);  // Lê a linha inteira
@@ -101,63 +113,79 @@ int main() {
             //cout << TC << " " << D << " " << P << endl;
             //cout << i << jobs[i].getTC() << "* " << jobs[i].getD() << " " << jobs[i].getP() << endl;
             }
+        
         //printjobs(jobs);//Debug
+        
         for(int t=0; t<T; t++){//Calcula lsf
-            for(int n=0; n<N; n++){//
-                //cout<<"---------------------------------"<<endl;
-                if(t%jobs[n].getD() == 0){
-                    jobs[n].setCount(jobs[n].getTC());
-                    //cout<< t<<"%"<<jobs[n].getD()<<"="<<t%jobs[n].getD()<<endl;
-                    }
-                if(jobs[n].getCount() > 0) {
-                    LSF[n]= jobs[n].getD() -( t%jobs[n].getD()) - jobs[n].getCount();
-                    //cout<<jobs[n].getD()<<"-("<<t%jobs[n].getD()<<")-"<<jobs[n].getCount()<<"="<<LSF[n]<<endl;
-                    }
-                else LSF[n]=0;
-                //cout<<n<<"-"<<LSF[n]<<endl;
-                //cout<<"---------------------------------"<<endl;
-            }
-            min_value = -1;
-            for (int j = 0; j < N; j++) {// Assume que o primeiro elemento menor é o primeiro n nulo
-                if (LSF[j] > 0) {
-                min_value = LSF[j];
-                p = j; 
-                break;
-                }
-            } 
-            for (int o= 0; o < N; o++) {
-                if (LSF[o] < min_value && LSF[o] > 0) { 
-                    min_value = LSF[o];  // Atualiza o menor valor encontrado
-                    p = o;  // Atualiza a posição do menor valor encontrado
-                    //cout<<"p"<<p<<endl;
-                    //cout<<"o"<<o<<endl;
-                }
-            }
-            //cout<<min_value<<endl;
-            for(int n=0; n<N; n++){
-                if(jobs[n].getCount() > 0 && LSF[n]==0){
-                    min_value = 1;
-                    p=n;
-                    break;
-                }
-            }
-            if (min_value > 0){// Verifica se tem algum job que ainda n tem seu count zerado, o q indica ainda se tem processos para rodar
-                //cout<<min_value<<endl;
-                if (c != 'A' + p || c == '.') troca++;// ab or .n   
-                if (c >='A' && c<= 'Z')b = c - 'A';
-                if (c != '.' && c != 'A' + p && jobs[b].getCount() > 0) premp++; 
-                jobs[p].setCount(jobs[p].getCount()-1);
-                //printjobs(jobs);
-                c = 'A' + p;
-                //cout<<c<<" - "<< t <<endl;
-                str += c;
-            }
-            else{
-                if (c != '.') troca++;// n.
-                c = '.';
-                str += c;
-            }
-	    }  
-    }
-return 0;
+		for(int n=0; n<N; n++){//
+			//cout<<"---------------------------------"<<endl;
+			if(t%jobs[n].getP() == 0){
+				jobs[n].setCount(jobs[n].getTC());
+				//cout<< t<<"%"<<jobs[n].getD()<<"="<<t%jobs[n].getD()<<endl;
+				}
+			if(jobs[n].getCount() > 0) {
+				LSF[n]= jobs[n].getD() -( t%jobs[n].getP()) - jobs[n].getCount();
+				//cout<<jobs[n].getD()<<"-("<<t%jobs[n].getD()<<")-"<<jobs[n].getCount()<<"="<<LSF[n]<<endl;
+				}
+			else LSF[n]=0;
+			//cout<<n<<"-"<<LSF[n]<<endl;
+			//cout<<"---------------------------------"<<endl;
+		}
+		min_value = -1;
+		for (int j = 0; j < N; j++) {// Assume que o primeiro elemento menor é o primeiro n nulo
+			if (LSF[j] > 0) {
+			min_value = LSF[j];
+			p = j; 
+			break;
+			}
+		} 
+		for (int o= 0; o < N; o++) {
+			if (LSF[o] < min_value && LSF[o] > 0) { 
+			    min_value = LSF[o];  // Atualiza o menor valor encontrado
+			    p = o;  // Atualiza a posição do menor valor encontrado
+			    //cout<<"p"<<p<<endl;
+			    //cout<<"o"<<o<<endl;
+			}
+		}
+		//cout<<min_value<<endl;
+		
+		for(int n=0; n<N; n++){
+			if(jobs[n].getCount() > 0 && LSF[n]==0){
+				min_value = 1;
+				p=n;
+				break;
+			}
+		}
+		
+		if (min_value > 0){// Verifica se tem algum job que ainda n tem seu count zerado, o q indica ainda se tem processos para rodar
+			//cout<<min_value<<endl;
+			if (c != 'A' + p || c == '.') troca++;// ab or .n   
+			if (c >='A' && c<= 'Z') b = c - 'A';
+			if (c == 'A' + p && jobs[b].getCount() == jobs[b].getTC()) troca++;// troca de contexto de A|A mesmo processo mas periodos diferentes
+			if(t>0){
+				if (c != '.' && c != 'A' + p && jobs[b].getCount() > 0){
+					premp++;
+					//str += 'p';
+					}
+					
+			}
+			jobs[p].setCount(jobs[p].getCount()-1);
+			//printjobs(jobs);
+			c = 'A' + p;
+			//cout<<c<<" - "<< t <<endl;
+			str += c;
+		}
+		else{
+			if (c != '.') {
+			troca++;
+			premp++;
+			//str += 'p';
+			}// n.
+			c = '.';
+			str += c;
+		}
+	}  
+}
+return 0;
+
 }
